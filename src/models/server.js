dfa9c26571c62b832fe89b0a04e581bb60a8b973
Diego Gonzales <diegoegj14@gmail.com>
2023-06-19 16:05:13 -0500

    sockets - basic concepts

--- src/models/server.js
+++ src/models/server.js
@@ -1,17 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import { createServer } from 'http';
+import { Server as Servidor } from 'socket.io';
+import { socketController } from '../sockets/controller.js';
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
     this.paths = {};
+    this.server = createServer(this.app);
+    this.io = new Servidor(this.server);
 
     // Middlewares
     this.middlewares();
 
     // Routes
     this.routes();
+
+    // Sockets config
+    this.sockets();
   }
 
   middlewares() {
@@ -26,8 +34,12 @@ class Server {
     // this.app.use(this.paths.auth, require('../routes/auth.routes'));
   }
 
+  sockets() {
+    this.io.on('connection', socketController);
+  }
+
   listen() {
-    this.app.listen(this.port, () => {
+    this.server.listen(this.port, () => {
       console.log('App listening on port', process.env.PORT);
     });
   }
